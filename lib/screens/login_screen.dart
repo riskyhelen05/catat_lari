c10c5962b2b7a199c902a309110e34f8897d59fa
Helen Risky Dwi Wahyuni <riskyhelen1@gmail.com>
2026-05-08 10:21:39 +0700

    STEP 5: create login UI

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Login")),
+      body: Padding(
+        padding: EdgeInsets.all(16),
+        child: Column(
+          children: [
+            TextField(decoration: InputDecoration(labelText: "Username")),
+            TextField(
+              decoration: InputDecoration(labelText: "Password"),
+              obscureText: true,
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {},
+              child: Text("Login"),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
